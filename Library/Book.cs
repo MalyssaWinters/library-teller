393f554266a08befde4a74f8eda260ae3e0caf30
Malyssa Winters <malyssawinters5@gmail.com>
2016-06-29 14:24:09 -0400

    Book, Magazine, DVD class

---
+++ Library/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book : Media
+    {
+        public override void PrintMediaDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
